40aca58e093a7fb3ff958162fd63ed50b0973550
Akanksha Kesari <akankshakesari02@gmail.com>
2021-07-28 07:16:40 -0500

    discovery-client-service first commit

---
+++ src/main/java/com/example/discoveryService/DiscoveryClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoveryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryClientServiceApplication.class, args);
+	}
+
+}
